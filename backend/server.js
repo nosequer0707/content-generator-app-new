179e05497072fb4ae208cec21e61cfec9bba2fe2
nosequer0707 <raul.xilon@gmail.com>
2025-04-09 18:14:53 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -70,4 +70,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta para servir el frontend
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
